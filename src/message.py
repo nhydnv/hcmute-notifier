1f58b39e51857dffc694204ea4491b5a2d51823a
mungkeumeomeo <nhido.1911@gmail.com>
2026-03-09 19:31:21 +1100

    sent complete formatted message via email

--- src/message.py
+++ src/message.py
@@ -6,13 +6,12 @@ import smtplib
 import os
 
 class Message:
-  def __init__(self, id, subject, body, create_date):
+  def __init__(self, id, subject, body, create_date, sender):
     self.id = id
     self.subject = subject
     self.body = body
-    try:
-      self.create_date = datetime.fromisoformat(create_date)
-    except ValueError: self.create_date = None
+    self.sender = sender
+    self.create_date = create_date
 
   def is_new(self):
     now = datetime.now()
@@ -22,10 +21,22 @@ class Message:
   def send(self, to_addr):
     from_addr = os.environ['SENDER']
     msg = EmailMessage()
-    msg['Subject'] = 'subject'
+    msg['Subject'] = f'[HCMUTE Portal] New Message: {self.subject}'
     msg['FROM'] = from_addr
     msg['TO'] = to_addr
-    msg.set_content('content')
+
+    header = f'<p><strong>From:</strong> {self.sender}</p>' \
+             f'<p><strong>Date:</strong> {self.create_date.strftime('%d/%m/%y')}</p>' \
+             f'<p><strong>Subject:</strong> {self.subject}</p>'
+    
+    self.body = f'{header}<hr>{self.body}'
+
+    # Plain text fallback
+    plain_text = BeautifulSoup(self.body, 'html.parser').get_text()
+    msg.set_content(plain_text)
+
+    # HTML content
+    msg.add_alternative(self.body, subtype='html')
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
       server.login(
@@ -39,11 +50,14 @@ class Message:
 
 class GeneralMessage(Message):
   def __init__(self, msg):
-    super().__init__(msg['MaTin'], msg['TieuDe'], msg['NoiDung'], msg['CreateDate'])
+    try:
+      create_date = datetime.fromisoformat(msg['CreateDate'])
+      super().__init__(msg['MaTin'], msg['TieuDe'], msg['NoiDung'], create_date, msg['StaffName'])
+    except ValueError: self.create_date = None
 
 class StudentMessage(Message):
   def __init__(self, msg):
-    super().__init__(msg['MessageID'], msg['MessageSubject'], msg['MessageBody'], msg['CreationDate'])
-
-m = GeneralMessage(getGeneralMessages()[0])
-m.send('mungkeumeomeo@gmail.com')
+    try:
+      create_date = datetime.strptime(msg['CreationDate'], "%d/%m/%Y")
+      super().__init__(msg['MessageID'], msg['MessageSubject'], msg['MessageBody'], create_date, msg['SenderName'])
+    except ValueError: self.create_date = None
